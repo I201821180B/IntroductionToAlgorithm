fa905aea9e2253a947b5a7cdb41b90166b15fe42
LiunxPailsley <LiunxAlkaid@outlook.com>
2017-09-30 10:09:00 +0800

    增加新函数
    增加了获取随机数组和打印数组的函数

---
+++ common/common/common.h
@@ -0,0 +1,38 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "benchmark.h"
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <random>
+#include <functional>
+
+using namespace std;
+
+void getRandomVector(vector<int>& v, size_t size, int from, int to)
+{
+	v.resize(0);
+	std::default_random_engine generator(time(NULL));
+	std::uniform_int_distribution<int> dis(from, to);
+	auto dice = std::bind(dis, generator);
+
+	for (int i = 0; i < size; ++i)
+	{
+		v.push_back(dice());
+		//cout << v.back() << " ";
+	}
+}
+
+template<typename T>
+void printVector(vector<T>& v)
+{
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
+#endif
