3f408a3b0b50377fef16e2e644aadf6cee8bf757
LiunxPaisley <liunxalkaid@outlook.com>
2017-10-07 09:58:46 +0800

    去除多余文件

--- QuickSort/QuickSort/QuickSort.h
+++ QuickSort/QuickSort/QuickSort.h
@@ -1,10 +1,10 @@
 #ifndef _QUICKSORT_H_
 #define _QUICKSORT_H_
 
-#include <iostream>
 #include <vector>
 
 using namespace std;
+
 void swap(int& i, int& j);
 void quickSort(vector<int>& A, int p, int r);
 int partition(vector<int>& A, int p, int r);
--- QuickSort/QuickSort/main.cpp
+++ QuickSort/QuickSort/main.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main()
 {
 	vector<int> v;
-	getRandomVector(v, 10, 0, 100);
+	getRandomVector(v, 100000, 0, 1000000);
 
-	printVector(v);
+	//printVector(v);
 
 	CPUTimer clk;
 	clk.Start();
 	quickSort(v, 0, v.size() - 1);
 	clk.Stop();
-	printVector(v);
+	//printVector(v);
 	cout << "QuickSort time: " << clk.microsecond() << " us." << endl;
 }
