a9ca8cf6671be186def7b51c6bb405e8bbd0a0d5
LiunxPaisley <liunxalkaid@outlook.com>
2017-10-07 09:58:14 +0800

    增加了删除文件的命令

--- del.bat
+++ del.bat
@@ -2,4 +2,11 @@
 del /q /s *.exe
 del /q /s *.db
 del /q /s *.ipch
+del /q /s *.tlog
+del /q /s *.pdb
+del /q /s *.log
+del /q /s *.idb
+del /q /s *.obj
+del /q /s *.ilk
+del /q /s *.lastbuildstate
 echo fuck off
