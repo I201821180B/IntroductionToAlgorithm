ed103015a76de240b62a6f43adc15ab6dab06e4d
LiunxPaisley <liunxalkaid@outlook.com>
2017-10-07 09:57:39 +0800

    计数排序

---
+++ CountingSort/CountingSort/CountingSort.h
@@ -0,0 +1,11 @@
+#ifndef _COUNTINGSORT_H_
+#define _COUNTINGSORT_H_
+
+#include <vector>
+
+using namespace std;
+
+void countSort(const vector<int>& A, vector<int>& B, const int k);
+
+
+#endif
--- CountingSort/CountingSort/main.cpp
+++ CountingSort/CountingSort/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <vector>
 
+#include "CountingSort.h"
 #include "common.h"
 
 using namespace std;
 
 int main()
 {
-	vector<int> v;
-	getRandomVector(v, 100000, 0, 1000000);
+	int size = 100000;
+	int max = 1000000;
 
+	vector<int> v;
+	getRandomVector(v, size, 0, max);
+	vector<int> vec(v.size(), 0);
+
+	//printVector(v);
+	CPUTimer clk;
+	clk.Start();
+	countSort(v, vec, max);
+	clk.Stop();
+	cout << "CountingSort time: " << clk.microsecond() << " us." << endl;
+	//printVector(vec);
+	//getchar();
 }
