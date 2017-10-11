adfc30bd6f8bde5353bf0e0bcb8f444126aaba89
LiunxPaisley <liunxalkaid@outlook.com>
2017-10-11 16:41:01 +0800

    堆排序

---
+++ BucketSort/BucketSort/BucketSort.h
@@ -0,0 +1,10 @@
+#ifndef _BUCKETSORT_H_
+#define _BUCKETSORT_H_
+
+#include <vector>
+
+using namespace std;
+
+void bucketSort(vector<int>& v, const int max);
+
+#endif
---
+++ BucketSort/BucketSort/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+#include "common.h"
+#include "BucketSort.h"
+
+using namespace std;
+
+int main()
+{
+	const int64_t size = 100000;
+	const int64_t max = 1000000;
+	vector<int> v;
+	getRandomVector(v, size, 0, max);
+
+	//printVector(v);
+
+	CPUTimer clk;
+	clk.Start();
+	bucketSort(v, max);
+	clk.Stop();
+	//printVector(v);
+
+	cout << "BucketSort time: " << clk.microsecond() << " us." << endl;
+}
