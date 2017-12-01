82662201c540d2914c4cb089cab865512afebb09
LiunxPaisley <liunxalkaid@outlook.com>
2017-12-01 17:43:30 +0800

    活动选择问题

--- IV Advanced Design and Analysis Techniques/Greedy Algorithms/activitySelector/activitySelector/activitySelector.cpp
+++ IV Advanced Design and Analysis Techniques/Greedy Algorithms/activitySelector/activitySelector/activitySelector.cpp
@@ -1,4 +1,11 @@
 #include "activitySelector.h"
 
-void recursiveActivitySelector();
-void greedyActivitySelector();
+void recursiveActivitySelector(const vector<int32_t>& _s, const vector<int32_t>& _f, size_t _k, size_t _n)
+{
+
+}
+
+void greedyActivitySelector()
+{
+
+}
--- IV Advanced Design and Analysis Techniques/Greedy Algorithms/activitySelector/activitySelector/activitySelector.h
+++ IV Advanced Design and Analysis Techniques/Greedy Algorithms/activitySelector/activitySelector/activitySelector.h
@@ -1,6 +1,14 @@
 #ifndef _ACTIVITY_SELECTOR_H_
 
-void recursiveActivitySelector();
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+void recursiveActivitySelector(const vector<int32_t>& _s, const vector<int32_t>& _f, size_t k, size_t n);
 void greedyActivitySelector();
 
 #endif // !_ACTIVITY_SELECTOR_H_
